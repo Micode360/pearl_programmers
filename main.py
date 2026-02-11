9dfadbd4c349bb8d60a11cbddfcbd9725649571b
Miracle King Hesed <abayemiracle@gmail.com>
2026-02-11 12:54:03 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def introduction(str):
+    return str
+
+
+# Write your introduction
+miracle = introduction("Miracle is the name. Programming is the game.")
+
+
+
+# Print introduction
+print(miracle)
