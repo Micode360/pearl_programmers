62b1ccddba7bfc50b261ee159303cd4ccbfe0fa7
Tori <stevevicky16@gmail.com>
2026-02-11 13:21:12 +0100

    vicky added her introduction

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ def introduction(str):
 # Write your introduction
 miracle = introduction("Miracle is the name. Programming is the game.")
 williams = introduction("My name is williams and i code with a bang.")
-
+vicky = introduction("My name is vicky and programming is great.")
 
 
 # Print introduction
 print(miracle)
 print(williams)
+print(vicky)
